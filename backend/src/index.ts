69624ab650a345ea7d890a98f614cfa11aabe0cf
Tarjmul Qasim <tarjmulqasim@gmail.com>
2026-04-18 23:50:37 +0530

    Render Port Fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -742,7 +742,10 @@ server.delete(
   },
 );
 
-server.listen({ port: 3001 }, (err, address) => {
+server.listen({
+  port: Number(process.env.PORT) || 3001,
+  host: '0.0.0.0' 
+ }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
